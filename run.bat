97b1d34616bf6b74030455a9a60078edae2321d0
zhemingbuhuichong <126929929+zhemingbuhuichong@users.noreply.github.com>
2024-03-03 21:50:25 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,8 +4,8 @@ set /a n=0
 :cycle
 echo ---------------------------------------------------------------------------------
 set /a n+=1
-echo 正在进行第%n%次尝试
+echo 脮媒脭脷陆酶脨脨碌脷%n%麓脦鲁垄脢脭
 echo.
 .\main.exe
-timeout /t 2
+timeout /t 1
 goto :cycle
